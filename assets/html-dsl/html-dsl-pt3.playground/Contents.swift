dddbbe83c083d62bf37342ca7cb10fc2e573f69d
Brandon Williams <mbw234@gmail.com>
2017-06-28 15:57:02 -0400

    stuff

--- assets/html-dsl/html-dsl-pt3.playground/Contents.swift
+++ assets/html-dsl/html-dsl-pt3.playground/Contents.swift
@@ -1,229 +1,3 @@
-
-precedencegroup Semigroup { associativity: left }
-infix operator <>: Semigroup
-protocol Semigroup {
-  static func <>(lhs: Self, rhs: Self) -> Self
-}
-protocol Monoid: Semigroup {
-  static var e: Self { get }
-}
-extension Array: Monoid {
-  static var e: Array<Element> { return  [] }
-  static func <>(lhs: Array, rhs: Array) -> Array {
-    return lhs + rhs
-  }
-}
-
-func concat<M: Monoid>(_ xs: [M]) -> M {
-  return xs.reduce(M.e, <>)
-}
-extension Array where Element: Monoid {
-  func concat() -> Element {
-    return self.reduce(Element.e, <>)
-  }
-}
-
-
-
-struct Attribute {
-  let key: String
-  let value: String
-
-  init(_ key: String, _ value: String) {
-    self.key = key
-    self.value = value
-  }
-}
-
-struct AttributeKey<A> {
-  let key: String
-  init(_ key: String) { self.key = key }
-}
-
-struct Element {
-  let name: String
-  let attribs: [Attribute]
-  let children: [Node]?
-
-  init(_ name: String, _ attribs: [Attribute], _ children: [Node]?) {
-    self.name = name
-    self.attribs = attribs
-    self.children = children
-  }
-}
-
-enum Node {
-  case element(Element)
-  case text(String)
-}
-
-let verboseDoc: Node = .element(
-  .init("header", [],  [
-    .element(
-      .init("h1", [.init("id", "welcome")],  [
-        .text("Welcome!")
-        ])
-    ),
-    .element(
-      .init("p", [],  [
-        .text("Welcome to you, who has come here. See "),
-        .element(
-          .init("a", [.init("href", "/more"), .init("remove-me", "true")], [
-            .text("more")
-            ])
-        ),
-        .text(".")
-        ])
-    )]
-  )
-)
-
-func node(_ name: String, _ attribs: [Attribute], _ children: [Node]?) -> Node {
-  return .element(.init(name, attribs, children))
-}
-
-func node(_ name: String, _ children: [Node]?) -> Node {
-  return node(name, [], children)
-}
-
-func h1(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("h1", attribs, children)
-}
-
-func h1(_ children: [Node]) -> Node {
-  return h1([], children)
-}
-
-func p(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("p", attribs, children)
-}
-
-func p(_ children: [Node]) -> Node {
-  return p([], children)
-}
-
-
-func a(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("a", attribs, children)
-}
-
-func a(_ children: [Node]) -> Node {
-  return a([], children)
-}
-
-func header(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("header", attribs, children)
-}
-
-func header(_ children: [Node]) -> Node {
-  return header([], children)
-}
-
-func ul(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("ul", attribs, children)
-}
-
-func ul(_ children: [Node]) -> Node {
-  return ul([], children)
-}
-
-func li(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("li", attribs, children)
-}
-
-func li(_ children: [Node]) -> Node {
-  return li([], children)
-}
-
-func footer(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("footer", attribs, children)
-}
-
-func footer(_ children: [Node]) -> Node {
-  return footer([], children)
-}
-
-func span(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("span", attribs, children)
-}
-
-func span(_ children: [Node]) -> Node {
-  return span([], children)
-}
-
-func html(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("html", attribs, children)
-}
-
-func html(_ children: [Node]) -> Node {
-  return html([], children)
-}
-
-func body(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("body", attribs, children)
-}
-
-func body(_ children: [Node]) -> Node {
-  return body([], children)
-}
-
-func menu(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("menu", attribs, children)
-}
-
-func menu(_ children: [Node]) -> Node {
-  return menu([], children)
-}
-
-func main(_ attribs: [Attribute], _ children: [Node]) -> Node {
-  return node("main", attribs, children)
-}
-
-func main(_ children: [Node]) -> Node {
-  return main([], children)
-}
-
-infix operator =>
-func => <A> (key: AttributeKey<A>, value: A) -> Attribute {
-  return .init(key.key, "\(value)")
-}
-
-let id = AttributeKey<String>("id")
-let href = AttributeKey<String>("href")
-
-extension Node: ExpressibleByStringLiteral {
-  init(stringLiteral value: String) {
-    self = .text(value)
-  }
-}
-
-func render(node: Node) -> String {
-  switch node {
-  case let .element(e):
-    return render(element: e)
-  case let .text(t):
-    return t
-  }
-}
-
-func render(element: Element) -> String {
-  let openTag = "<\(element.name)"
-  let openTagWithAttrs = openTag
-    + (element.attribs.isEmpty ? "" : " ")
-    + render(attributes: element.attribs)
-    + ">"
-  let children = (element.children ?? []).map(render(node:)).joined()
-  let closeTag = element.children == nil ? "/>" : "</\(element.name)>"
-
-  return openTagWithAttrs + children + closeTag
-}
-
-func render(attributes: [Attribute]) -> String {
-  return attributes
-    .map { attr in "\(attr.key)=\"\(attr.value)\"" }
-    .joined(separator: " ")
-}
-
 struct View<D, N: Monoid> {
   let view: (D) -> N
 
@@ -248,6 +22,7 @@ struct FooterData {
   let email: String
   let twitter: String
 }
+
 let footerContent = View<FooterData, [Node]> {
   [
     ul([
@@ -352,12 +127,16 @@ func >>> <A, B, C>(_ f: @escaping (A) -> B, g: @escaping (B) -> C) -> (A) -> C {
   return { g(f($0)) }
 }
 
-let homepageView: View<HomepageData, [Node]> =
-  headerContent.map(header >>> pure).contramap { _ in () }
-    <> articlesList.contramap { $0.articles }
-    <> footerContent.map(footer >>> pure).contramap { $0.footerData }
+let siteHeader = headerContent.map(header >>> pure)
+let mainArticles = articlesList.map(main >>> pure)
+let siteFooter = footerContent.map(footer >>> pure)
 
-let homepage_v2 = homepageView
+let combined: View<HomepageData, [Node]> =
+  siteHeader.contramap { _ in () }
+    <> mainArticles.contramap { $0.articles }
+    <> siteFooter.contramap { $0.footerData }
+
+let homepage_v2 = combined
   .map(main >>> pure)
   .map { nodes in
     [
@@ -369,25 +148,22 @@ let homepage_v2 = homepageView
     ]
 }
 
-struct LayoutData<D> {
-  let data: D
+struct SiteLayoutData<D> {
+  let contentData: D
   let footerData: FooterData
 }
 
-func layout<D>(content: View<D, [Node]>) -> View<LayoutData<D>, [Node]> {
-
+func siteLayout<D>(content: View<D, [Node]>) -> View<SiteLayoutData<D>, [Node]> {
   return (
     headerContent.map(header >>> pure).contramap { _ in () }
-      <> content.map(main >>> pure).contramap { $0.data }
+      <> content.map(main >>> pure).contramap { $0.contentData }
       <> footerContent.map(footer >>> pure).contramap { $0.footerData }
     )
     .map(body >>> pure >>> html >>> pure)
 }
 
-
-
-let __data = LayoutData(
-  data: [
+let siteData = SiteLayoutData(
+  contentData: [
     Article(date: "Jun 22, 2017", title: "Type-Safe HTML in Swift"),
     Article(date: "Feb 17, 2015", title: "Algebraic Structure and Protocols"),
     Article(date: "Jan 6, 2015", title: "Proof in Functions"),
@@ -399,10 +175,7 @@ let __data = LayoutData(
   )
 )
 
-render(
-  view: layout(content: articlesList),
-  with: __data
-)
+render(view: siteLayout(content: articlesList), with: siteData)
 
 
 
---
+++ assets/html-dsl/html-dsl-pt3.playground/Sources/HTML.swift
@@ -0,0 +1,193 @@
+precedencegroup Semigroup { associativity: left }
+infix operator <>: Semigroup
+public protocol Semigroup {
+  static func <>(lhs: Self, rhs: Self) -> Self
+}
+public protocol Monoid: Semigroup {
+  static var e: Self { get }
+}
+extension Array: Monoid {
+  public static var e: Array<Element> { return  [] }
+  public static func <>(lhs: Array, rhs: Array) -> Array {
+    return lhs + rhs
+  }
+}
+
+public struct Attribute {
+  public let key: String
+  public let value: String
+
+  public init(_ key: String, _ value: String) {
+    self.key = key
+    self.value = value
+  }
+}
+
+public struct AttributeKey<A> {
+  public let key: String
+  init(_ key: String) { self.key = key }
+}
+
+public struct Element {
+  public let name: String
+  public let attribs: [Attribute]
+  public let children: [Node]?
+
+  public init(_ name: String, _ attribs: [Attribute], _ children: [Node]?) {
+    self.name = name
+    self.attribs = attribs
+    self.children = children
+  }
+}
+
+public enum Node {
+  case element(Element)
+  case text(String)
+}
+
+public func node(_ name: String, _ attribs: [Attribute], _ children: [Node]?) -> Node {
+  return .element(.init(name, attribs, children))
+}
+
+public func node(_ name: String, _ children: [Node]?) -> Node {
+  return node(name, [], children)
+}
+
+public func h1(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("h1", attribs, children)
+}
+
+public func h1(_ children: [Node]) -> Node {
+  return h1([], children)
+}
+
+public func p(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("p", attribs, children)
+}
+
+public func p(_ children: [Node]) -> Node {
+  return p([], children)
+}
+
+
+public func a(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("a", attribs, children)
+}
+
+public func a(_ children: [Node]) -> Node {
+  return a([], children)
+}
+
+public func header(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("header", attribs, children)
+}
+
+public func header(_ children: [Node]) -> Node {
+  return header([], children)
+}
+
+public func ul(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("ul", attribs, children)
+}
+
+public func ul(_ children: [Node]) -> Node {
+  return ul([], children)
+}
+
+public func li(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("li", attribs, children)
+}
+
+public func li(_ children: [Node]) -> Node {
+  return li([], children)
+}
+
+public func footer(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("footer", attribs, children)
+}
+
+public func footer(_ children: [Node]) -> Node {
+  return footer([], children)
+}
+
+public func span(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("span", attribs, children)
+}
+
+public func span(_ children: [Node]) -> Node {
+  return span([], children)
+}
+
+public func html(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("html", attribs, children)
+}
+
+public func html(_ children: [Node]) -> Node {
+  return html([], children)
+}
+
+public func body(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("body", attribs, children)
+}
+
+public func body(_ children: [Node]) -> Node {
+  return body([], children)
+}
+
+public func menu(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("menu", attribs, children)
+}
+
+public func menu(_ children: [Node]) -> Node {
+  return menu([], children)
+}
+
+public func main(_ attribs: [Attribute], _ children: [Node]) -> Node {
+  return node("main", attribs, children)
+}
+
+public func main(_ children: [Node]) -> Node {
+  return main([], children)
+}
+
+infix operator =>
+public func => <A> (key: AttributeKey<A>, value: A) -> Attribute {
+  return .init(key.key, "\(value)")
+}
+
+public let id = AttributeKey<String>("id")
+public let href = AttributeKey<String>("href")
+
+extension Node: ExpressibleByStringLiteral {
+  public init(stringLiteral value: String) {
+    self = .text(value)
+  }
+}
+
+public func render(node: Node) -> String {
+  switch node {
+  case let .element(e):
+    return render(element: e)
+  case let .text(t):
+    return t
+  }
+}
+
+public func render(element: Element) -> String {
+  let openTag = "<\(element.name)"
+  let openTagWithAttrs = openTag
+    + (element.attribs.isEmpty ? "" : " ")
+    + render(attributes: element.attribs)
+    + ">"
+  let children = (element.children ?? []).map(render(node:)).joined()
+  let closeTag = element.children == nil ? "/>" : "</\(element.name)>"
+
+  return openTagWithAttrs + children + closeTag
+}
+
+public func render(attributes: [Attribute]) -> String {
+  return attributes
+    .map { attr in "\(attr.key)=\"\(attr.value)\"" }
+    .joined(separator: " ")
+}
+
